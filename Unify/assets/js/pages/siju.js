8c464160c34b7b3b058a852225afe5cca78f0568
carlos <carlos5atp@gmail.com>
2016-12-23 23:03:40 +0100

    init

---
+++ Unify/assets/js/pages/siju.js
@@ -0,0 +1,29 @@
+﻿function showHideActivation(option)
+{
+    if (option == "oneEmail") {
+        $('#oneEmailDiv').show();
+        $('#twoEmailDiv').hide();
+        $('#actionBtn1').css('color', '#41B5A7');
+        $('#actionBtn1').css('border', '1px solid #41B5A7');
+        $('#actionBtn2').css('color', '#11c1ff');
+        $('#actionBtn2').css('border', '1px solid #11c1ff');
+    }
+    else
+    {
+        $('#twoEmailDiv').show();
+        $('#oneEmailDiv').hide();
+        $('#actionBtn2').css('color', '#41B5A7');
+        $('#actionBtn2').css('border', '1px solid #41B5A7');
+        $('#actionBtn1').css('color', '#11c1ff');
+        $('#actionBtn1').css('border', '1px solid #11c1ff');
+    }
+    
+}
+function comprarOneEmail()
+{
+    console.log('asd')
+    AjaxCart.addproducttocart_catalog('/desplige/addproducttocart/catalog/4/1/1');
+}
+function comprarTwoEmail() {
+    AjaxCart.addproducttocart_catalog('/desplige/addproducttocart/catalog/3/1/1');
+}
