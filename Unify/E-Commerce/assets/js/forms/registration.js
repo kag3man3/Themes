21384f4c03180d8969749bf0d9e04f3154006922
carlos <carlos5atp@gmail.com>
2017-01-30 14:18:51 -0500

    30-1

---
+++ Unify/E-Commerce/assets/js/forms/registration.js
@@ -0,0 +1,35 @@
+﻿function setEmail()
+{
+    $('#Email').val($('#emailSiju').val() + '@siju.es');
+    if ($('#emailSiju').val()!='')
+        searchUserSiju($('#emailSiju').val());
+
+}
+function callOtherDomain() {
+    if (invocation) {
+        invocation.open('POST', url, true);
+        invocation.setRequestHeader('X-PINGOTHER', 'pingpong');
+        invocation.setRequestHeader('Content-Type', 'application/xml');
+        invocation.onreadystatechange = handler;
+        invocation.send(body);
+    }
+}
+function searchUserSiju(user) {
+    $.ajax({
+        url: 'http://192.168.65.200/WebSiteSiju/Service2.svc/searchUserSiju/' + user,
+        type: 'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        success: function (data) {
+            if (data == '1') {
+                $('#userStatus').css("color", "red");
+            }
+            else {
+                $('#userStatus').css("color", "blue");
+            }
+        },
+        error: function (jqXHR, status, error) {
+            alert('Disculpe, hay problemas de conexión a la bd');
+        }
+    });
+}
--- Unify/assets/js/pages/siju.js
+++ Unify/assets/js/pages/siju.js
@@ -1,31 +1,73 @@
-﻿function showHideActivation(option)
+﻿function enviarEmail()
 {
-    $('.g-popup-wrapper').show();
-    return;
-    if (option == "oneEmail") {
-        $('#oneEmailDiv').show();
-        $('#twoEmailDiv').hide();
-        $('#actionBtn1').css('color', '#FF3366');
-        $('#actionBtn1').css('border', '1px solid #FF3366');
-        $('#actionBtn2').css('color', '#3DC2FF');
-        $('#actionBtn2').css('border', '1px solid #3DC2FF');
+    name = $('#name').val();
+    email = $('#email').val();
+    message = $('#message').val();
+    $.ajax({
+        url: 'http://localhost:64736/Service2.svc/sendMailSijuSiju/'+name+'-'+email+'-'+message,
+        type: 'POST',
+        dataType: 'json',
+        success: function (data) {
+            alert("Email enviado");
+        },
+        error: function (jqXHR, status, error) {
+            alert('Disculpe, hay problemas de conexión a la bd');
+        }
+    });
+}
+
+function showHideActivation(option)
+{
+    if (isLogin()) {
+        $.ajax({
+            url: '../../addproducttocart/catalog/4/1/1/',
+            type: 'POST',
+            dataType: 'json',
+            success: function (datos) {
+                location.href = "./../checkout"
+            },
+            error: function (jqXHR, status, error) {
+                alert('Disculpe, hay problemas de conexión a la bd');
+            }
+        });
     }
     else
     {
-        $('#twoEmailDiv').show();
-        $('#oneEmailDiv').hide();
-        $('#actionBtn2').css('color', '#FF3366');
-        $('#actionBtn2').css('border', '1px solid #FF3366');
-        $('#actionBtn1').css('color', '#3DC2FF');
-        $('#actionBtn1').css('border', '1px solid #3DC2FF');
+        location.href = "./login";
     }
-    
+
+    //if (option == "oneEmail") {
+    //    $('#oneEmailDiv').show();
+    //    $('#twoEmailDiv').hide();
+    //    $('#actionBtn1').css('color', '#FF3366');
+    //    $('#actionBtn1').css('border', '1px solid #FF3366');
+    //    $('#actionBtn2').css('color', '#3DC2FF');
+    //    $('#actionBtn2').css('border', '1px solid #3DC2FF');
+    //}
+    //else
+    //{
+    //    $('#twoEmailDiv').show();
+    //    $('#oneEmailDiv').hide();
+    //    $('#actionBtn2').css('color', '#FF3366');
+    //    $('#actionBtn2').css('border', '1px solid #FF3366');
+    //    $('#actionBtn1').css('color', '#3DC2FF');
+    //    $('#actionBtn1').css('border', '1px solid #3DC2FF');
+    //}
 }
-function comprarOneEmail()
+function createUserSijuEmail()
 {
-    console.log('asd')
-    AjaxCart.addproducttocart_catalog('/addproducttocart/catalog/4/1/1');
+    $.ajax({
+        url: 'http://192.168.65.200/Service2.svc/createUserSiju/okok',
+        type: 'POST',
+        dataType: 'json',
+        success: function (data) {
+            alert("User creado");
+        },
+        error: function (jqXHR, status, error) {
+            alert('Disculpe, hay problemas de conexión a la bd');
+        }
+    });
 }
-function comprarTwoEmail() {
-    AjaxCart.addproducttocart_catalog('/addproducttocart/catalog/3/1/1');
-}
+//function comprarTwoEmail() {
+//    AjaxCart.addproducttocart_catalog('/addproducttocart/catalog/3/1/1');
+//}
